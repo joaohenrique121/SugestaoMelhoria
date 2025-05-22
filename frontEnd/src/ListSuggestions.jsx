9954b7d692a0c08f69fe78298a54a9f514634c51
joaohenrique121 <simionatojoaohenrique@gmail.com>
2025-05-22 15:52:37 -0300

    captação de dados do formulario e implementação da requisição fetch POST/GET

--- frontEnd/src/ListSuggestions.jsx
+++ frontEnd/src/ListSuggestions.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const ListSuggestions = () => {
+    useEffect(() => {
+        fetch('http://localhost:8069/api/sugestoes', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+    }, []);
     return (
         <div>
             <h1>Lista de Sugestões:</h1>
--- frontEnd/src/SendSuggestion.jsx
+++ frontEnd/src/SendSuggestion.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const SendSuggestion = () => {
+    const formref = useRef(null);
+
+    const sendSuggestion = async (e)=>{
+        e.preventDefault()
+        const nome = formref.current.nome.value
+        const descricao = formref.current.descricao.value
+        const response = await fetch('http://localhost:8069/api/sugestoes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                nome: nome,
+                descricao: descricao
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    console.error('Erro:', data.erro);
+                } else {
+                    console.log('Sucesso:', data);
+                }
+            })
+            .catch(error => console.error('Erro na requisição:', error));
+
+
+    }
+
     return (
-        <form className="max-w-md mx-auto bg-white shadow-md rounded-2xl p-6 space-y-4">
+        <form ref={formref} onSubmit={(e)=>sendSuggestion(e)} className="max-w-md mx-auto bg-white shadow-md rounded-2xl p-6 space-y-4">
             <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="nome">Nome</label>
                 <input type="text" name="nome" id="nome" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
             </div>
             <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="descricao">Descrição</label>
-                <input type="text" name="descricao" id="descricao" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
+                <textarea  name="descricao" id="descricao" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
             </div>
             <button type="submit" className="w-full bg-black text-white rounded-xl py-2 hover:bg-white hover:text-black transition-colors">Enviar</button>
         </form>
