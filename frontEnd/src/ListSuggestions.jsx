8a18b9b8249d164ded7aaf4b5431b458cf45d590
joaohenrique121 <simionatojoaohenrique@gmail.com>
2025-05-23 11:40:01 -0300

    mudanca na logica componentes, passagem prop para atualizacao de estado dinamicamente

--- frontEnd/src/ListSuggestions.jsx
+++ frontEnd/src/ListSuggestions.jsx
@@ -1,24 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
-const ListSuggestions = () => {
-    const [sugestoes, setSugestoes] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:8069/api/sugestoes', {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                if(data && data.sugestoes){
-                    setSugestoes(data.sugestoes)
-                }
+const ListSuggestions = ({sugestoes}) => {
 
-            })
-            .catch(err => console.error(err));
-    }, []);
     return (
         <div>
             <h1 className="text-center text-2xl font-bold mb-4">Lista de Sugestões:</h1>
@@ -32,13 +15,14 @@ const ListSuggestions = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    {sugestoes.map((item) => (
+                    {Array.isArray(sugestoes) && sugestoes.map((item) => (
                         <tr key={item.id} className="hover:bg-gray-100">
                             <td className="border border-black px-4 py-2">{item.nome}</td>
                             <td className="border border-black px-4 py-2">{item.descricao}</td>
                         </tr>
                     ))}
                     </tbody>
+
                 </table>
             </div>
 
--- frontEnd/src/SendSuggestion.jsx
+++ frontEnd/src/SendSuggestion.jsx
@@ -1,7 +1,21 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import ListSuggestions from "./ListSuggestions.jsx";
 
 const SendSuggestion = () => {
     const formref = useRef(null);
+    const [sugestoes, setSugestoes] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:8069/api/sugestoes')
+            .then(res => res.json())
+            .then(data => {
+                if (data && data.sugestoes) {
+                    setSugestoes(data.sugestoes);
+                    console.log(data.sugestoes)
+                }
+            });
+    }, []);
 
     const sendSuggestion = async (e)=>{
         e.preventDefault()
@@ -20,6 +34,11 @@ const SendSuggestion = () => {
         })
             .then(response => response.json())
             .then(data => {
+                if (data && data.sugestao) {
+                    setSugestoes(prevSugestoes => [...prevSugestoes, data.sugestao]);
+                    formref.current.nome.value = ""
+                    formref.current.descricao.value = ""
+                }
                 if (data.erro) {
                     console.error('Erro:', data.erro);
                 } else {
@@ -32,17 +51,20 @@ const SendSuggestion = () => {
     }
 
     return (
-        <form ref={formref} onSubmit={(e)=>sendSuggestion(e)} className="max-w-md mx-auto bg-white shadow-md rounded-2xl p-6 space-y-4">
-            <div>
-                <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="nome">Nome</label>
-                <input type="text" name="nome" id="nome" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
-            </div>
-            <div>
-                <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="descricao">Descrição</label>
-                <textarea  name="descricao" id="descricao" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
-            </div>
-            <button type="submit" className="w-full bg-black text-white rounded-xl py-2 hover:bg-white hover:text-black transition-colors">Enviar</button>
-        </form>
+        <>
+            <form ref={formref} onSubmit={(e)=>sendSuggestion(e)} className="max-w-md mx-auto bg-white shadow-md rounded-2xl p-6 space-y-4">
+                <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="nome">Nome</label>
+                    <input type="text" name="nome" id="nome" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
+                </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="descricao">Descrição</label>
+                    <textarea name="descricao" id="descricao" className="w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"/>
+                </div>
+                <button type="submit" className="w-full bg-black text-white rounded-xl py-2 hover:bg-white hover:text-black transition-colors">Enviar</button>
+            </form>
+            <ListSuggestions sugestoes={sugestoes}/>
+        </>
     );
 };
 
